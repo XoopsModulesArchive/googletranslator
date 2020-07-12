3716002eb562f0df4da96dbb13dd81a51479988b
mambax7 <mambax7@gmail.com>
2020-07-11 22:45:52 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ xoops_version.php
@@ -0,0 +1,17 @@
+<?php
+$modversion['name'] = "Google Translator";
+$modversion['version'] = 1.01;
+$modversion['description'] = "Use the Google Translator from within your XOOPS site.";
+$modversion['author'] = "Ported To Xoops By Danordesign";
+$modversion['credits'] = "";
+$modversion['help'] = "";
+$modversion['license'] = "GPL see LICENSE";
+$modversion['official'] = 0;
+$modversion['image'] = "images/googletranslator.gif";
+$modversion['dirname'] = "googletranslator";
+$modversion['hasAdmin'] = 0;
+$modversion['adminpath'] = "";
+$modversion['hasMain'] = 1;
+$modversion['templates'][1]['file'] = "googletranslator.html";
+$modversion['templates'][1]['description'] = "";
+?>
